5d3ac46b55045a258d7cffb687877b315056d2b5
JoyYing1014 <149246621+JoyYing1014@users.noreply.github.com>
2026-04-25 18:49:58 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+import sast as fs
+from sast.lightsecagg.FedUN_SecAgg_Client import FedUN_SecAgg_Client
+from sast.lightsecagg.FedUN_SecAgg_Server import FedUN_SecAgg_Server
+from sast.lightsecagg.FedAvg_SecAgg_Server import FedAvg_SecAgg_Server
+
+fs.Client = FedUN_SecAgg_Client
+fs.FedUN = FedUN_SecAgg_Server
+fs.FedAvg = FedAvg_SecAgg_Server
+
+if __name__ == '__main__':
+
+    my_task = fs.Fedunlearning()
+    # ================== 拦截验证测试 ==================
+    print("\n" + "=" * 50)
+    print("【拦截验证】当前使用的算法类是:", type(my_task.algorithm))
+    print("【拦截验证】当前使用的客户端类是:", type(my_task.algorithm.client_list[0]))
+    print("=" * 50 + "\n")
+    # =================================================
+    my_task.run()
